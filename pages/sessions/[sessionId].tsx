4d7ca4d9f6128944fe12dc249987a99179d16088
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-10-22 15:44:11 +0200

    wip

--- pages/sessions/[sessionId].tsx
+++ pages/sessions/[sessionId].tsx
@@ -92,10 +92,7 @@ export default function Session() {
     });
 
     socket.on('update', (socketData) => {
-      if (socketData.id !== data?.id) {
-        return;
-      }
-      setData(data);
+      setData((data) => (data?.id === socketData.id ? socketData : data));
     });
 
     if (sessionId) {
@@ -228,8 +225,8 @@ export default function Session() {
                             className="w-4 h-4"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
                               d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
                             />
                           </svg>
